ac7737194a4fd60b4ba9ecd01c6043e3de243176
Adriano <adriano@grus.local>
2022-10-07 14:50:45 -0300

    willpopscop adicionado no camera_page

--- lib/ui/camera/camera_view_controller.dart
+++ lib/ui/camera/camera_view_controller.dart
@@ -1,15 +1,34 @@
 import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
 
 class CameraViewController{
-  final List<String> pathList = [];
+  
 
+  Future<bool> showConfirmationDialogExitWithoutSaving(BuildContext context, bool condition){
+    
+  if(condition){
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text("Deseja sair sem salvar as fotos"),
+            actions: [
+              TextButton(onPressed: ()=> Navigator.pop(context), child: const Text('Cancelar')),
+              TextButton(
+                onPressed: (){
+                  Navigator.pop(context);
+                  Navigator.pop(context);
+                  }, child: const Text('Sair')),
+            ],
+           );
+         },
+      );
+    return Future.value(false);
+    }
+    return Future.value(true);
 
-  void addImage(String path){
-    pathList.add(path);
   }
 
-  void removeImage(int index){
-    pathList.removeAt(index);
-  }
+
 
 }
